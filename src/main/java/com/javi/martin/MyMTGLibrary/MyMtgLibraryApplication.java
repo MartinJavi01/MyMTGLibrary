347614455cd1e999ef341e8694d9942a85e84acb
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-06-03 22:56:55 +0200

    Initial commit

---
+++ src/main/java/com/javi/martin/MyMTGLibrary/MyMtgLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.javi.martin.MyMTGLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMtgLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMtgLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javi/martin/MyMTGLibrary/MyMtgLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javi.martin.MyMTGLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMtgLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
