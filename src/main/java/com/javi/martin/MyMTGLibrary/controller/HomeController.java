0bf2badaaa900faba88298c60b877210d5e0a406
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-06-24 16:03:10 +0200

    feat001: Created first html and css files necessary for basic ui usage

---
+++ src/main/java/com/javi/martin/MyMTGLibrary/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.javi.martin.MyMTGLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/", "/home"})
+public class HomeController {
+
+    @GetMapping
+    public String toHome(Model model) {
+        model.addAttribute("message", "hola wenas tardes");
+        return "home";
+    }
+}
