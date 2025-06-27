496e6d0941d0d8b3073b2cdbedce2d04a0cfc2f3
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-06-27 19:43:47 +0200

    0.1: Show card symbols in ui view

---
+++ src/main/java/com/javi/martin/MyMTGLibrary/constants/MyMTGLibraryConstants.java
@@ -0,0 +1,9 @@
+package com.javi.martin.MyMTGLibrary.constants;
+
+public class MyMTGLibraryConstants {
+
+    public static final String SYMBOL_API_BASE_URL = " http://eakett.ca/mtgimage/symbol";
+    public static final String SVG_EXTENSION = ".svg";
+    public static final String MANA_PATH = "/mana/";
+    public static final String OTHER_PATH = "/other/";
+}
--- src/main/java/com/javi/martin/MyMTGLibrary/service/CardUIPreparer.java
+++ src/main/java/com/javi/martin/MyMTGLibrary/service/CardUIPreparer.java
@@ -2,9 +2,14 @@ package com.javi.martin.MyMTGLibrary.service;
 
 import com.javi.martin.MyMTGLibrary.dto.MTGCardDTO;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
+import java.util.Objects;
 
+import static com.javi.martin.MyMTGLibrary.constants.MyMTGLibraryConstants.*;
+
+
+//http://eakett.ca/mtgimage/
 @Service
 public class CardUIPreparer {
 
@@ -14,18 +19,34 @@ public class CardUIPreparer {
         var splitedDescription = cardToPrepare.getOracleText().split("\\.");
         finalCard.setOracleText(String.join(".<br>", splitedDescription));
 
-        finalCard.setTypeLine(addPreBlankSpace(finalCard.getTypeLine(), "Card type:"));
-        finalCard.setCmc(addPreBlankSpace(finalCard.getCmc(), "Cmc:"));
-        finalCard.setOracleText(addPreBlankSpace(cardToPrepare.getOracleText(), "Description"));
+        finalCard.setOracleText(getUIDescription(finalCard.getOracleText()));
 
         return finalCard;
     }
 
-    private String addPreBlankSpace(String s, String preString) {
-        var spaces = "";
-        if (preString.length() < 15) {
-            spaces = "&nbsp;".repeat(15 - preString.length());
+    private String getUIDescription(String baseDescription) {
+        var returnString = "";
+        var splitedDescription = baseDescription.split("}");
+
+        for (int i = 0; i < splitedDescription.length - 1; i++) {
+            var insideKeysText = splitedDescription[i].split("\\{");
+            if (!insideKeysText[0].isEmpty()) {
+                returnString += insideKeysText[0];
+            }
+            returnString += "<img class=\"symbolImage\" src=\"" + getSymbolApiUrl(insideKeysText[1].toLowerCase()) + "\"/>";
         }
-        return spaces + s;
+
+        returnString += splitedDescription[splitedDescription.length - 1];
+
+        return returnString;
+    }
+
+    private String getSymbolApiUrl(String symbol) {
+        if ((!symbol.contains("b") && !symbol.contains("g") && !symbol.contains("w")
+                && !symbol.contains("r") && !symbol.contains("u")) && !StringUtils.isNumeric(symbol)) {
+            return SYMBOL_API_BASE_URL + OTHER_PATH + symbol + SVG_EXTENSION;
+        } else {
+            return SYMBOL_API_BASE_URL + MANA_PATH + symbol.replace("/","") + SVG_EXTENSION;
+        }
     }
 }
