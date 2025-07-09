2dfdf3315c387fcbe350d023661f190781431729
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-07-09 22:31:32 +0200

    (0.2): Finish save-update-delete flow

--- src/main/resources/static/js/details/cardDetails.js
+++ src/main/resources/static/js/details/cardDetails.js
@@ -1,10 +1,9 @@
 var currentCard = document.getElementById("imageDiv").dataset.currentCard;
-var copies = document.getElementById("copiesInput").dataset.copies;
 
 async function updateCardDetails(saved) {
     var currentCopies;
 
-    if (document.getElementById("copiesInput").checkVisibility) {
+    if (document.getElementById("copiesInput") !== null) {
         currentCopies = document.getElementById("copiesInput").value;
         if (!saved) {
             currentCopies = -1
@@ -16,20 +15,23 @@ async function updateCardDetails(saved) {
         currentCopies = 1;
     }
 
-    print("aqui: " + currentCard)
-    fetch('http://localhost:8010/update-card?copies=' + currentCopies, {
+    await fetch('http://localhost:8010/update-card?copies=' + currentCopies, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: currentCard
+        body: currentCard,
+        show: false,
         })
         .then(() => {
             if (saved) {
                 alert("Card copies updated successfully");
-            } else {
+            } else if (!saved && currentCopies > 0){
                 alert("Card saved on DB");
+            } else {
+                alert("Card deleted from DB");
             }
+            window.location.reload();
         })
         .catch((error) => {
         console.error('There was an error updating the DB:', error);
@@ -37,5 +39,7 @@ async function updateCardDetails(saved) {
 }
 
 function setInputValue() {
-    document.getElementById("copiesInput").value = copies;
+    if (document.getElementById("copiesInput") !== null) {
+        document.getElementById("copiesInput").value = document.getElementById("copiesInput").dataset.copies;
+    }
 }
