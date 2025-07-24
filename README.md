04ff14609b8889df6380a688a99e6b0e792880df
Javier MartÃín Escobar <javiermartinescobar.contact@gmail.com>
2025-07-24 18:09:09 +0200

    Update project version and delete readme

--- README.md
+++
@@ -1,7 +0,0 @@
-# MyMTGLibrary
-Project for storing all your MTG cards easelly with mongoDB
-
-## Version definition
-- 0.1 -> Display card details, add double sided cards support.
-- 0.2 -> Add UI options to save, delete your cards. Preview all cards.
-- 0.3 -> Add filters to all cards preview. Add usable search engine for online cards.
--- src/main/java/com/javi/martin/MyMTGLibrary/constants/MyMTGLibraryConstants.java
+++ src/main/java/com/javi/martin/MyMTGLibrary/constants/MyMTGLibraryConstants.java
@@ -2,7 +2,7 @@ package com.javi.martin.MyMTGLibrary.constants;
 
 public class MyMTGLibraryConstants {
 
-    public static final String CURRENT_VERSION = "0.1.0";
+    public static final String CURRENT_VERSION = "0.2.0";
 
     public static final String SCRYFALL_BASE_PATH = "https://api.scryfall.com/";
     public static final String NAMED_PATH = "cards/named";
