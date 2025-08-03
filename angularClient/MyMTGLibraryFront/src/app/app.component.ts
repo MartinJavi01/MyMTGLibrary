613bbdc9fa165b16f8c623c7b6a099ef3d63f551
Javier MartÃín Escobar <javiermartinescobar.contact@gmail.com>
2025-08-03 12:24:28 +0200

    (0.4): WIP angular migration

--- angularClient/MyMTGLibraryFront/src/app/app.component.ts
+++ angularClient/MyMTGLibraryFront/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyMTGLibraryFront';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    var wallpaperNum = Math.floor(Math.random() * (10 - 0 + 1));
+    this.renderer.setStyle(document.body, 'background-image', "url(\"wallpapers/mtg-wallpaper-" + wallpaperNum + ".jpg\")");
+    this.renderer.setStyle(document.body, 'background-size', "contain");
+  }
 }
