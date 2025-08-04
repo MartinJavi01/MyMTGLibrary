00ab8a1f23a522465b7d35ccb259f8624fe51685
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-08-04 23:22:24 +0200

    (0.4): Add card details page and home redirects

--- angularClient/MyMTGLibraryFront/src/app/search-bar/search-bar.component.ts
+++ angularClient/MyMTGLibraryFront/src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterOutlet, RouterLink, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [RouterModule],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
 
+  optionText: String;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.optionText = 'Collection';
+  }
+
+  redirectAndChangeOptionText(){
+    this.optionText = (this.optionText === 'Collection') ? 'Home' : 'Collection';
+    if (this.optionText === 'Home') {
+      this.router.navigate(['collection'], { relativeTo: this.route });
+    } else {
+      this.router.navigate(['home'], { relativeTo: this.route });
+    }
+  }
+
+  redirect(redirectPath: String) {
+    this.router.navigate([redirectPath], { relativeTo: this.route });
+  }
 }
