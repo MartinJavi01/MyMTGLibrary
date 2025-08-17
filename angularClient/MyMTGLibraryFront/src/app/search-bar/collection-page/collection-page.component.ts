4052ac8e654eacd76fd9bdd78ed9d3e2cc28b562
Javier Martín <javiermartinescobar.contact@gmail.com>
2025-08-17 22:05:37 +0200

    (0.4): End collection filters base front structure

--- angularClient/MyMTGLibraryFront/src/app/search-bar/collection-page/collection-page.component.ts
+++ angularClient/MyMTGLibraryFront/src/app/search-bar/collection-page/collection-page.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './collection-page.component.css'
 })
 export class CollectionPageComponent {
+  filtersOpened = false;
 
+  changeFiltersOpened() {
+    this.filtersOpened = !this.filtersOpened;
+  }
 }
